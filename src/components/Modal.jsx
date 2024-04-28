ec3cc08807e1389f28eef9efb4ff3b06cd280a1e
himanshu sharma <sharmahimanshu6478@gmail.com>
2024-04-28 23:45:47 +0530

    issue fixed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = ({ isOpen, Close , children }) => {
   return (
     <>
       {isOpen && (
-        <div className="model w-screen h-full  absolute top-0">
+        <div className="model w-screen h-full  absolute top-0 left-0">
         <div className="modal-box w-3/4 lg:w-1/3 h-auto flex justify-start space-y-3 items-center flex-col  text-gray bg-white min-h-[200px] min-w-[200px] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 p-4">
           <div
             onClick={() => Close()}
